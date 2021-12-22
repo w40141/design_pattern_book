b6a89aa0556b5291387a57430c5f9dc36dd879a8
w40141 <w.40141@gmail.com>
2021-12-22 23:16:45 +0900

    finish sec_02

---
+++ java/sec_02/Print2.java
@@ -0,0 +1,4 @@
+public abstract class Print2 {
+    public abstract void printWeak();
+    public abstract void printStrong();
+}
---
+++ java/sec_02/PrintBanner2.java
@@ -0,0 +1,17 @@
+public class PrintBanner2 extends Print2 {
+    private Banner banner;
+
+    public PrintBanner2(String string) {
+        this.banner = new Banner(string);
+    }
+
+    @Override
+    public void printWeak() {
+        banner.showWithParen();
+    }
+
+    @Override
+    public void printStrong() {
+        banner.showWithAster();
+    }
+}
