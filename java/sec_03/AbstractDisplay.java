020d9a78948d221a1a0d000ac53b0bd69be9eec5
w40141 <w.40141@gmail.com>
2021-12-23 22:47:57 +0900

    finish sec_03

---
+++ java/sec_03/AbstractDisplay.java
@@ -0,0 +1,13 @@
+public abstract class AbstractDisplay {
+    public void open();
+    public void print();
+    public void close();
+
+    public default void display() {
+        open();
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+        close();
+    }
+}
---
+++ java/sec_03/StringDisplay.java
@@ -0,0 +1,31 @@
+public class StringDisplay extends AbstractDisplay {
+
+    private String string;
+    private int width;
+
+    public StringDisplay(String string) {
+        this.string = string;
+        width = string.length();
+    }
+
+    public void open() {
+        printLine();
+    }
+
+    public void print() {
+        System.out.println("|" + string + "|");
+    }
+
+    public void close() {
+        printLine();
+    }
+
+    private void printLine() {
+        System.out.print("+");
+        for (int i = 0; i < width; i++) {
+            System.out.print("-");
+        } 
+        System.out.println("+");
+    }
+
+}
