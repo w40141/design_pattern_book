f8d5384b461ee1ca07ae65a479a939004d60b125
w40141 <w.40141@gmail.com>
2022-11-17 13:42:18 +0900

    writing sec_08

---
+++ java/sec_08/factory/Factory.java
@@ -0,0 +1,24 @@
+package factory;
+
+/**
+ * Factory
+ */
+public abstract class Factory {
+  public static Factory getFactory(String className) {
+    Factory factory = null;
+    try {
+      factory = (Factory) Class.forName(className).getDeclaredConstructor().newInstance();
+    } catch (ClassNotFoundException e) {
+      System.out.println("クラス " + className + " が見つかりません。");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return factory;
+  }
+
+  public abstract Link createLink(String caption, String url);
+
+  public abstract Tray createTray(String caption);
+
+  public abstract Page createPage(String title, String author);
+}
