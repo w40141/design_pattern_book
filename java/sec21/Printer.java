1235d408b3c90f3ee464d6c96221bc1178e09032
w40141 <w.40141@gmail.com>
2023-01-17 15:24:01 +0900

    finish sec21

---
+++ java/sec21/Printer.java
@@ -0,0 +1,41 @@
+public class Printer implements Printable{
+    private String name;
+    public Printer() {
+        heavyJob("Printerのインスタンスを生成中");
+    }
+
+    public Printer(String name) {
+        this.name = name;
+        heavyJob("Printerのインスタンス（" + name + "）を生成中");
+    }
+
+    @Override
+    public void setPrinterName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getPrinterName() {
+        return name;
+    }
+
+    @Override
+    public void print(String string) {
+        System.out.println("=== " + name + " ===");
+        System.out.println(string);
+    }
+
+    private void heavyJob(String msg) {
+        System.out.println(msg);
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+
+            }
+            System.out.print(".");
+        }
+        System.out.println("完了。");
+    }
+}
---
+++ java/sec21/PrinterProxy.java
@@ -0,0 +1,39 @@
+public class PrinterProxy implements Printable {
+    private String name;
+    private Printer real;
+
+    public PrinterProxy() {
+        this.name = "No Name";
+        this.real = null;
+    }
+
+    public PrinterProxy(String name) {
+        this.name = name;
+        this.real = null;
+    }
+
+    @Override
+    public synchronized void setPrinterName(String name) {
+        if (real != null) {
+            real.setPrinterName(name);
+        }
+        this.name = name;
+    }
+
+    @Override
+    public synchronized String getPrinterName() {
+        return name;
+    }
+
+    @Override
+    public void print(String string) {
+        realize();
+        real.print(string);
+    }
+
+    private synchronized void realize() {
+        if(real == null) {
+            real = new Printer(name);
+        }
+    }
+}
