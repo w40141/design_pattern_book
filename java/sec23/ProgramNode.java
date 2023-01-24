927d6d4f9be164d150800af703bc801daff8aafe
w40141 <w.40141@gmail.com>
2023-01-24 17:12:55 +0900

    finish sec23

---
+++ java/sec23/ProgramNode.java
@@ -0,0 +1,15 @@
+public class ProgramNode extends Node {
+	private Node commandListNode;
+
+	@Override
+	public void parse(Context context) throws ParseException {
+		context.skipToken("program");
+		commandListNode = new CommandListNode();
+		commandListNode.parse(context);
+	};
+
+	@Override
+	public String toString() {
+		return "[program " + commandListNode + "]";
+	}
+}
